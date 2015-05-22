c8ebf28d47e9da5a76a29b2b6cb750e56d78533f
ueg1990 <uehtesham90@gmail.com>
2015-05-22 03:06:37 -0400

    Add route for users

--- hotline/app.py
+++ hotline/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask.ext import restful
-from resources.sms import SMS
 from resources.hello_world import HelloWorld
+from resources.sms import SMS
+from resources.users import Users
 
 app = Flask(__name__)
 api = restful.Api(app)
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Users, '/users')
 api.add_resource(SMS, '/sms')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
