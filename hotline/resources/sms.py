a309693fe841cef185960a48410f610502c59e21
ueg1990 <uehtesham90@gmail.com>
2015-05-26 02:29:07 -0400

    Update routes to work with MongoDB

--- hotline/resources/sms.py
+++ hotline/resources/sms.py
@@ -1,13 +1,15 @@
 import os
+import pickle
 
 from flask import request, Response
 from flask.ext import restful
 from twilio import twiml
-
-from resources.users import DB
-from common.time_zone import current_time_zone_info
-
 from twilio.rest import TwilioRestClient
+
+from common.time_zone import current_time_zone_info
+from db.db_mongo import mongo_db
+#from db.db_redis import redis_client
+# from resources.users import DB
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
@@ -17,20 +19,26 @@ class SMS(restful.Resource):
 
     def post(self):
         response = twiml.Response()
-        body_msg = request.form['Body']
+        body_msg = 'hello' # request.form['Body']
         if _forward_sms(body_msg):
             reply_msg = "Your message has been received. Someone will be with you shortly!!!"
-            response.sms(reply_msg)
-            return Response(str(response), content_type='text/xml')
+        else:
+            reply_msg = "No one on duty currently!!!"
+        response.sms(reply_msg)
+        return Response(str(response), content_type='text/xml')
 
 def _forward_sms(body_msg):
     client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     current_date, current_time = current_time_zone_info()
     current_time = int(current_time)
-    for time, volunteer in DB.items():
-        if time[0] == current_date:
-            if time[1] > time[2] and (current_time < time[1] and current_time < time[2]):
-                return client.messages.create(to=volunteer['phone_number'], from_=TWILIO_NUMBER, body=body_msg)
-            elif time[1] < time[2] and (time[1] < current_time < time[2]):
-                return client.messages.create(to=volunteer['phone_number'], from_=TWILIO_NUMBER, body=body_msg)
-    return None
+    volunteer_count = 0
+    for document in mongo_db.contacts.find():
+        time_from, time_to = document['time_from'], document['time_to']
+        if document['date'] == current_date:
+            if time_from > time_to and (current_time < time_from and current_time < time_to):
+                client.messages.create(to=document['phone_number'], from_=TWILIO_NUMBER, body=body_msg)
+                volunteer_count += 1
+            elif time_from < time_to and (time_from < current_time < time_to):
+                client.messages.create(to=document['phone_number'], from_=TWILIO_NUMBER, body=body_msg)
+                volunteer_count += 1
+    return volunteer_count
--- hotline/resources/users.py
+++ hotline/resources/users.py
@@ -1,13 +1,16 @@
 import csv
 import os
+import json 
 
 from flask import request, jsonify
 from flask.ext import restful 
 from flask.ext.restful import reqparse
 from werkzeug import secure_filename
 
+from db.db_mongo import mongo_db 
+from db.db_redis import redis_client 
+
 UPLOAD_FOLDER = '/tmp/'
-DB = {}
 
 class Users(restful.Resource):
 
@@ -18,9 +21,10 @@ class Users(restful.Resource):
         with open(os.path.join(UPLOAD_FOLDER, file_name)) as f:
             csv_f = csv.reader(f)
             next(csv_f, None)
-                # DB.update({(data[3], int(data[4]), int(data[5])) : {'first_name': data[0], 'last_name': data[1], 'phone_number': data[2]} for data in csv_f})
             for data in csv_f:
                 try:
-                    DB.update({(data[3], int(data[4]), int(data[5])) : {'first_name': data[0], 'last_name': data[1], 'phone_number': data[2]}})
+                    # DB.update({(data[3], int(data[4]), int(data[5])) : {'first_name': data[0], 'last_name': data[1], 'phone_number': data[2]}})
+                    # redis_client.set((data[3], int(data[4]), int(data[5])), {'first_name': data[0], 'last_name': data[1], 'phone_number': data[2]})
+                    mongo_db.contacts.insert({'first_name': data[0], 'last_name': data[1], 'phone_number': data[2], 'date' : data[3], 'time_from' : int(data[4]), 'time_to' : int(data[5])})
                 except ValueError:
                     pass
